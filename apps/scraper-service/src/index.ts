e4e16b3928498b9dc6a4455e208eac7c2d5b2801
kysley <evan@kysley.com>
2022-03-12 01:18:31 -0500

    make it work

---
+++ apps/scraper-service/src/index.ts
@@ -0,0 +1,122 @@
+import Puppeteer from "puppeteer";
+import * as cheerio from "cheerio";
+import Fastify, { FastifyRequest } from "fastify";
+import FastifyCors from "fastify-cors";
+
+const cache = new Map();
+let page: Puppeteer.Page;
+
+async function crawlTheme(uri: string) {
+  await page.goto(`https://vscodethemes.com/e/${uri}`);
+
+  const content = await page.content();
+
+  const $ = cheerio.load(content);
+
+  const examples = $(".css-swve6m svg.css-ahoycg").toArray();
+
+  let idx = 0;
+  return examples.map((elem) => {
+    const dict = extractColourDict(elem.childNodes);
+    const title = extractName($, idx);
+
+    console.log(dict);
+
+    const sortedDict = Object.keys(dict).sort((key1, key2) =>
+      dict[key1] > dict[key2] ? 1 : -1
+    );
+
+    idx++;
+
+    return {
+      title,
+      dict: sortedDict,
+    };
+  });
+}
+
+function extractName($: cheerio.CheerioAPI, idx: number) {
+  // @ts-expect-error
+  return $(".css-1s2194k")[idx].children[0].nodeValue;
+}
+
+function extractColourDict(texts: cheerio.Node[]) {
+  const ddict: Record<string, number> = {};
+
+  texts.forEach((textEl, idx) => {
+    //@ts-expect-error
+    const tspans = [...textEl.childNodes];
+
+    // Skip the comment colour
+    if (idx === 8) return;
+
+    tspans.forEach((tspan) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      const styleAttr = tspan.attribs.style as string;
+      const hex = styleAttr.split(";")[0].substring(6);
+      if (!ddict[hex]) {
+        ddict[hex] = 0;
+      }
+      ddict[hex] += 1;
+    });
+  });
+
+  return ddict;
+}
+
+const fastify = Fastify({ trustProxy: true });
+
+fastify.register(FastifyCors, {
+  origin: [
+    "http://localhost:8800",
+    "https://bpc.e8y.fun",
+    "http://localhost:3000",
+  ],
+});
+
+type PeerManagerGetRequest = FastifyRequest<{
+  Querystring: {
+    search: string;
+  };
+}>;
+fastify.get("/", async (req: PeerManagerGetRequest, res) => {
+  const { search } = req.query;
+  if (!search) return res.status(404).send();
+
+  const uri = encodeURI(search.toLowerCase());
+
+  if (cache.has(uri)) {
+    res.send(cache.get(uri));
+    return;
+  }
+
+  const resp = await crawlTheme(uri);
+  cache.set(uri, resp);
+
+  res.send(resp);
+});
+
+fastify.get("/health", async () => {
+  // Random search that only has one theme
+  // this is a super fragile health test tbfh
+  const resp = await crawlTheme(encodeURI("Taarú Teranga"));
+
+  if (resp["#000000"] !== 32) {
+    return false;
+  }
+  return true;
+});
+
+const start = async () => {
+  try {
+    const browser = await Puppeteer.launch();
+    page = await browser.newPage();
+    await fastify.listen(3600);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ apps/scraper-web/src/App.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+  import "./global.css";
+
+  let pairs;
+  let loading;
+
+  function submit(e: any) {
+    const formData = new FormData(document.querySelector("form"));
+    const data = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+    e.preventDefault();
+    getColours(data.theme);
+  }
+
+  async function getColours(theme: string) {
+    loading = true;
+    const res = await fetch(`http://localhost:3600/?search=${theme}`);
+    const data = await res.json();
+    loading = false;
+    pairs = data;
+  }
+</script>
+
+<header>
+  <h1>bracket buddy</h1>
+</header>
+<main>
+  <section>
+    <p>
+      Get the best bracket colour pair suggestions for your current theme 🎨
+    </p>
+    <ol>
+      <li>
+        Find your theme on <a href="https://vscodethemes.com">vscodethemes</a> and
+        "view extension"
+      </li>
+      <li>
+        Copy everything after the e/ in the url <code
+          >https://vscodethemes.com/<u>e/</u><span
+            >dracula-theme.theme-dracula</span
+          ></code
+        >
+      </li>
+      <li>Paste it below!</li>
+    </ol>
+    <form on:submit|preventDefault={submit}>
+      <input name="theme" type="text" placeholder="theme name" />
+      <button type="submit">get bracket colours</button>
+    </form>
+  </section>
+  <section>
+    {#if loading}
+      loading...
+    {/if}
+    {#if pairs}
+      <h2>found {pairs.length} variants for your theme!</h2>
+      <div class="grid">
+        {#each pairs as pair}
+          <div>
+            <h4>{pair.title}</h4>
+            {#each pair.dict as color}
+              <div>
+                <span style={`color: ${color}`}>{color}</span>
+              </div>
+            {/each}
+          </div>
+        {/each}
+      </div>
+    {/if}
+  </section>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    background-color: black;
+    color: white;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    gap: 2rem;
+    /* grid-template-columns: 1fr 1fr 1fr; */
+  }
+
+  header {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  code {
+    background-color: #212121;
+    padding: 0.25rem 0.5rem;
+    margin: 0.1rem;
+    border-radius: 3px;
+  }
+
+  code span {
+    color: green;
+  }
+
+  h1 {
+    color: white;
+    text-transform: uppercase;
+    font-size: 1.8rem;
+    font-weight: 700;
+    max-width: 14rem;
+    padding: 0;
+    margin: 0;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
